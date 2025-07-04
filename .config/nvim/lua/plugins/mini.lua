0f198559781480bef56dc19cf0dbb0e9d68ea7bc
Joshua McCarthy <1986983+digitalclover@users.noreply.github.com>
2025-07-04 10:21:53 +0900

    Added missing mini plugins

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,5 +3,8 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.icons').setup()
+    require('mini.move').setup()
+    require('mini.surround').setup()
+    require('mini.cursorword').setup()
   end,
 }
