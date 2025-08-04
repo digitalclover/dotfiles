9b2db92f2c99f3a6486bdf0c5534a1090bb88e2b
Joshua McCarthy <1986983+digitalclover@users.noreply.github.com>
2025-08-05 00:47:46 +0900

    Updated copilot-chat & config

--- .config/nvim/lua/plugins/copilot_chat.lua
+++ .config/nvim/lua/plugins/copilot_chat.lua
@@ -2,22 +2,20 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      { 'github/copilot.vim' },
       { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
     cmd = 'CopilotChat',
     opts = function()
-      vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-      })
       vim.g.copilot_no_tab_map = true
       local user = vim.env.USER or 'User'
       user = user:sub(1, 1):upper() .. user:sub(2)
       return {
         auto_insert_mode = true,
-        question_header = '  ' .. user .. ' ',
-        answer_header = '  Copilot ',
+        headers = {
+          user = '  ' .. user .. ' ',
+          assistant = '  Copilot ',
+          tool = '  Tool',
+        },
         window = {
           width = 0.4,
         },
@@ -25,7 +23,7 @@ return {
     end,
     keys = {
       { '<c-s>', '<CR>', ft = 'copilot-chat', desc = 'Submit Prompt', remap = true },
-      { '<leader>a', '', desc = '+ai', mode = { 'n', 'v' } },
+      { '<leader>a', '', desc = '+[A]I (Copilot)', mode = { 'n', 'v' } },
       {
         '<leader>aa',
         function()
@@ -54,36 +52,11 @@ return {
         mode = { 'n', 'v' },
       },
       {
-        '<leader>ab',
-        function()
-          local input = vim.fn.input 'Quick Chat: '
-          if input ~= '' then
-            require('CopilotChat').ask(input)
-          end
-        end,
-        desc = 'CopilotChat - Open Buffer Chat',
-        mode = { 'n', 'v' },
-      },
-      {
-        '<leader>ap',
-        function()
-          return require('CopilotChat').prompts()
-        end,
-        desc = 'CopilotChat - Prompt actions',
-      },
-      {
         '<leader>am',
         function()
           return require('CopilotChat').select_model()
         end,
         desc = 'CopilotChat - Select Model',
-      },
-      {
-        '<leader>ag',
-        function()
-          return require('CopilotChat').select_agent()
-        end,
-        desc = 'CopilotChat - Select Agent',
       },
       {
         '<leader>as',
