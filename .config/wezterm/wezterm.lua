ef491e42d0720adc92feb120914e215c1d4c042e
Joshua McCarthy <1986983+digitalclover@users.noreply.github.com>
2026-06-24 11:45:47 +0900

    Clean up

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require("wezterm")
-local background = require("backgrounds")
 local mux = wezterm.mux
 
 local config = wezterm.config_builder()
-local backgrounds = require("backgrounds")
 
 function tab_title(tab_info)
 	local title = tab_info.tab_title
@@ -13,22 +11,12 @@ function tab_title(tab_info)
 	return tab_info.active_pane.title
 end
 
-function generateRandomIndex(backgrounds)
-	local len = #backgrounds
-	if len == 0 then
-		return 0
-	end
-	local index = math.floor(math.random(len))
-	return index
-end
-
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
 	local title = tab_title(tab)
 	title = wezterm.truncate_right(title, max_width - 2)
 end)
 
 config.enable_wayland = true
-config.show_close_tab_button_in_tabs = false
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.tab_max_width = 64
@@ -56,8 +44,6 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_domain = "Debian"
 end
 
-local bg_index = generateRandomIndex(backgrounds)
--- config.background = backgrounds[1]
 config.window_background_opacity = 0.95
 
 wezterm.on("gui-startup", function(cmd)
